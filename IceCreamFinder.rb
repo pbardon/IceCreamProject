d6eb7fe4320a3b5b4179381075f686f45e98c6d5
Peter Bardon <pbard42@uw.edu>
2014-06-23 15:55:16 -0700

    --Added display of each shop with directions:

--- IceCreamFinder.rb
+++ IceCreamFinder.rb
@@ -19,11 +19,11 @@ end
 $address = "1061 Market St, San Francisco, CA"
 
 geolocationaddress = Addressable::URI.new(
-                                          :scheme => "http",
-                                          :host => "maps.googleapis.com",
-                                          :path => "maps/api/geocode/json",
-                                          :query_values => {address: $address}
-                                        ).to_s
+      :scheme => "http",
+      :host => "maps.googleapis.com",
+      :path => "maps/api/geocode/json",
+      :query_values => {address: $address}
+).to_s
 
 
 output = JSON.parse(RestClient.get(geolocationaddress))
@@ -39,17 +39,66 @@ puts coordinates
 
 
 search_address = Addressable::URI.new(
-  :scheme => "https",
-  :host => "maps.googleapis.com",
-  :path => "maps/api/place/nearbysearch/json",
-  :query_values => {key: $api_key, location: coordinates, keyword: "Ice Cream", types: 'food', radius: '1000', sensor: false}
+    :scheme => "https",
+    :host => "maps.googleapis.com",
+    :path => "maps/api/place/nearbysearch/json",
+    :query_values => { 
+                       key: $api_key,
+                       location: coordinates,
+                       keyword: "Ice Cream",
+                       types: 'food',
+                       radius: '1000',
+                       sensor: false
+                     }
 ).to_s
-p search_address
+
 
 search_output = JSON.parse(RestClient.get(search_address))
 
 
+shopcoords = []
+shop_names = []
+nearby_shops = search_output["results"]
 
-nearby_shops = search_output["results"].map{|result| result["name"]}
+nearby_shops.each do |shop|
+  shop_location = shop["geometry"]["location"]
+  shop_lat = shop_location["lat"]
+  shop_long = shop_location["lng"]
+  shop_names << shop["name"]
+  shopcoords << "#{shop_lat},#{shop_long}"
+end
 
-puts nearby_shops
+
+
+
+shopcoords.each_with_index do |coord, index|
+
+  directions_address = Addressable::URI.new(
+      :scheme => "https",
+      :host => "maps.googleapis.com",
+      :path => "maps/api/directions/json",
+      :query_values => {
+                        origin: coordinates,
+                        destination: coord
+                      }
+  ).to_s
+
+
+  dir_output = JSON.parse(RestClient.get(directions_address))
+
+  route = dir_output['routes'].first
+  steps = route['legs'].first['steps']
+  
+  puts shop_names[index]
+  puts "______________________"
+  puts
+  instructions = steps.map do |step|
+    parsed_html = Nokogiri::HTML(step['html_instructions']).text
+  end
+  instructions.each {|step| puts step}
+  puts
+end
+
+
+
+  
